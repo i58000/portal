c8c5c48ac70b41e1e619d3f50a7f213d908f0b75
i58000 <i58000@qq.com>
2020-08-29 22:21:53 +0800

    1

--- portal-server/src/ws/index.ts
+++ portal-server/src/ws/index.ts
@@ -3,7 +3,7 @@ import { verifyClient, getContext } from './context'
 
 import onMessage from './onMessage'
 import onConnection from './onConnection'
-import onclose from './onclose'
+import onClose from './onClose'
 
 const server = new WebSocket.Server({ port: 3002, verifyClient });
 
@@ -18,7 +18,7 @@ server.on('connection', (client: WebSocket.Client) => {
 
 
     client.on('close', event => {
-        onclose(ctx)
+        onClose(ctx)
     })
 
     // client.send('init msg');
